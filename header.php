284868bcab7376ea9617fd5e83105612f4c27aa7
Joel Friesen <joel@atomiccrayon.com>
2015-07-15 16:58:44 -0700

    bug fix - optional titles and descriptions

--- header.php
+++ header.php
@@ -49,18 +49,18 @@
   <?php endif; ?>
   
     </div>
-    <?php if ( get_theme_mod('title_display') ) : ?>
+    <?php if ( get_theme_mod('description_display') &&  get_theme_mod('title_display') ) : ?>
     <div class="col span3">
       <h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+      <h2 class="site-description"><?php bloginfo( 'description' ); ?></h2>
     </div>
     <?php elseif ( get_theme_mod('description_display') ) : ?>
     <div class="col span3">
       <h2 class="site-description"><?php bloginfo( 'description' ); ?></h2>
     </div>
-    <?php elseif ( get_theme_mod('description_display') &&  get_theme_mod('title_display') ) : ?>
+    <?php elseif ( get_theme_mod('title_display') ) : ?>
     <div class="col span3">
       <h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-      <h2 class="site-description"><?php bloginfo( 'description' ); ?></h2>
     </div>
     <?php endif; ?>
   </div>
