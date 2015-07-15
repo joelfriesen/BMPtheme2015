4f8978eb18858831436f1f0602e15fb1247233b2
Joel Friesen <joel@atomiccrayon.com>
2015-07-15 16:13:04 -0700

    layout and bug fixes
    Footer has proper spacing, some vertical rhythm with col padding

--- footer.php
+++ footer.php
@@ -1,6 +1,6 @@
     </div>
-	<div class="footer main container">
-		<div class="foottext">
+	<div class="footer main container col4">
+		<div class="foottext col span4">
 			<p>Footer</p>
 		</div>
 	</div>
